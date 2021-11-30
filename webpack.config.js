2a06a921d62bbe9c6fe6aa05bb4c0bd3324b1cc4
fabianofrank <fabianofrank@protonmail.com>
2021-11-30 12:36:17 -0300

    Add plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-};
-
-module.exports = {
-  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
 };
